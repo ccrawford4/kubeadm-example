e4c735fdd26df6f5a825f1e3bd5ba4d2adb0449f
ccrawford4 <calumcrawford9@gmail.com>
2025-08-30 18:52:24 -0700

    chore(firewall): open up more ports for nodes to join the cluster and web access

--- terraform/stacks/vms/firewall.tf
+++ terraform/stacks/vms/firewall.tf
@@ -1,19 +1,20 @@
-resource "google_compute_firewall" "http_nodeport" {
-  name    = "http-nodeport-firewall"
-  network = google_compute_network.application_network.name
-
-  allow {
-    protocol = "icmp"
-  }
-
-  allow {
-    protocol = "tcp"
-    ports    = ["30007"]
-  }
-
-  source_tags = ["application"]
-}
-
 resource "google_compute_network" "application_network" {
   name = "application-network"
 }
+
+resource "google_compute_firewall" "http_nodeport" {
+    name    = "http-nodeport-firewall"
+    network = google_compute_network.application_network.name
+
+    allow {
+      protocol = "icmp"
+    }
+
+    allow {
+      protocol = "tcp"
+      ports    = ["22", "80", "443", "6443", "30007"]
+    }
+
+    source_ranges = ["0.0.0.0/0"]
+    target_tags   = ["application"]
+  }
